386950b442bd2064f8fa0a3486fd17e8c28fb764
layanmeta <layan.meta@gmail.com>
2022-05-23 15:14:25 +0300

    Player movement and shooting

---
+++ Invaders/Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public PlayerProjectiles weapon;
+
+    public float horizontalSpeed = 2.0f;
+    public float edge = 0;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        Instantiate(this.weapon, this.transform.position, Quaternion.identity); 
+    }
+
+    private void FixedUpdate()
+    {
+        float movex = Input.GetAxis("Horizontal") * horizontalSpeed * Time.fixedDeltaTime;
+        transform.position += new Vector3(movex, 0);
+        if (transform.position.x >= edge)
+        {
+            transform.position = new Vector3(edge, transform.position.y, 0);
+        }
+        else if (transform.position.x <= -edge)
+        {
+            transform.position = new Vector3(-edge, transform.position.y, 0);
+        }
+    }
+}
---
+++ Invaders/Assets/Scripts/PlayerProjectiles.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProjectiles : MonoBehaviour
+{
+    public GameObject projectile;
+
+    public int damage = 40;
+    public int speed = -5;
+    public void Update()
+    {
+        transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Destroy(collision.gameObject);
+            Destroy(projectile);           
+        }
+        else if (collision.gameObject.tag == "Border")
+        {
+            Destroy(projectile);
+        }
+    }
+}
